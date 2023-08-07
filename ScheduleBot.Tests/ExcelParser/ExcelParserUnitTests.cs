f99c215f82cbd3250fa840371ffb8cf8432e9f56
WinWins <winwins@danimatcorp.com>
2023-08-07 19:52:45 +0300

    Обновлены тесты парсера

--- ScheduleBot.Tests/ExcelParser/ExcelParserUnitTests.cs
+++ ScheduleBot.Tests/ExcelParser/ExcelParserUnitTests.cs
@@ -1,55 +1,43 @@
 using System;
-using System.Collections.Generic;
-using ScheduleBot.ExcelParser.Enums;
-using ScheduleBot.ExcelParser.Models;
 using ScheduleBot.ExcelParser.Tools;
 using Xunit;
 using FluentAssertions;
+using ScheduleBot.Resources.Enums;
 
 namespace ScheduleBot.Tests.ExcelParser;
 
 public class ExcelParserUnitTests
 {
     [Theory]
-    [InlineData("ФИЗИКА лаб. 1-414 Горбунов, Радченко", 
-        2, 
-        (DayOfWeek)3, 
-        LessonType.All,
+    [InlineData("ФИЗИКА лаб. 1-414 Горбунов, Радченко",
         "ФИЗИКА лаб.",
         "Горбунов, Радченко",
         "1-414")]
     [InlineData("ЦИФРОВЫЕ ИЗМЕРИТЕЛЬНЫЕ СИСТЕМЫ лаб. I 3-309 Максимов ЯЗЫКИ ПРОГРАММИР. С УПРАВЛЯЕМЫМ КОДОМ лаб. II 3-320 Белова",
-        4, 
-        (DayOfWeek)5, 
-        LessonType.Numerator,
         "ЦИФРОВЫЕ ИЗМЕРИТЕЛЬНЫЕ СИСТЕМЫ лаб. I, ЯЗЫКИ ПРОГРАММИР. С УПРАВЛЯЕМЫМ КОДОМ лаб. II",
         "Максимов, Белова",
         "3-309, 3-320")]
-    [InlineData("ФИЗИЧЕСКАЯ КУЛЬТУРА И СПОРТ  к.2", 
-        3, 
-        (DayOfWeek)5, 
-        LessonType.All,
+    [InlineData("ФИЗИЧЕСКАЯ КУЛЬТУРА И СПОРТ  к.2",
         "ФИЗИЧЕСКАЯ КУЛЬТУРА И СПОРТ",
         "",
         "к.2")]
-    [InlineData("ИНОСТРАННЫЙ ЯЗЫК упр. к.1 Власко, Волхонская, Колотова", 
-        2, 
-        (DayOfWeek)1, 
-        LessonType.All,
+    [InlineData("ИНОСТРАННЫЙ ЯЗЫК упр. к.1 Власко, Волхонская, Колотова",
         "ИНОСТРАННЫЙ ЯЗЫК упр.",
         "Власко, Волхонская, Колотова",
         "к.1")]
-    [InlineData("ИНЖЕНЕРНАЯ ГРАФИКА лаб. I 1-309 Сулина II 1-311 Вяткин", 
-        4, 
-        (DayOfWeek)2, 
-        LessonType.Denominator,
+    [InlineData("ИНЖЕНЕРНАЯ ГРАФИКА лаб. I 1-309 Сулина II 1-311 Вяткин",
         "ИНЖЕНЕРНАЯ ГРАФИКА лаб. I, II",
         "Сулина, Вяткин",
         "1-309, 1-311")]
-    public void ExcelParser_ParseCellData_ShouldSuccess(string text, int para, DayOfWeek dayOfWeek, LessonType type, 
+    public void ExcelParser_ParseCellData_ShouldSuccess(string text,
         string expectedName, string expectedTeacher, string expectedLocation)
     {
-        // Arrange, Act
+        // Arrange
+        var para = Random.Shared.Next(1, 5);
+        var dayOfWeek = (DayOfWeek)Random.Shared.Next(1, 7);
+        var type = (LessonType)Enum.GetValues<LessonType>().GetValue(Random.Shared.Next(0, 3))!;
+        
+        // Act
         var lesson = Parser.ParseLesson(text, para, dayOfWeek, type);
         
         // Assert
