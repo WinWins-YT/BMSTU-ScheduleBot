0aa6539617f61b8bc318735f2a7c7158b46667cc
WinWins <winwins@danimatcorp.com>
2023-08-20 18:04:32 +0300

    Рефактор Utilities.cs

--- ScheduleBot.Resources/Tools/Utilities.cs
+++ ScheduleBot.Resources/Tools/Utilities.cs
@@ -1,28 +1,27 @@
-namespace ScheduleBot.Resources.Tools
+namespace ScheduleBot.Resources.Tools;
+
+/// <summary>
+/// Класс с общими утилитами
+/// </summary>
+internal static class Utilities
 {
     /// <summary>
-    /// Класс с общими утилитами
+    /// Конвертер номера пары во время начала пары
     /// </summary>
-    internal static class Utilities
+    /// <param name="para">Номер пары</param>
+    /// <returns>Время начала пары</returns>
+    internal static TimeOnly ParaToStartTime(int para)
     {
-        /// <summary>
-        /// Конвертер номера пары во время начала пары
-        /// </summary>
-        /// <param name="para">Номер пары</param>
-        /// <returns>Время начала пары</returns>
-        internal static TimeOnly ParaToStartTime(int para)
+        return para switch
         {
-            return para switch
-            {
-                1 => new TimeOnly(8, 30),
-                2 => new TimeOnly(10, 20),
-                3 => new TimeOnly(12, 10),
-                4 => new TimeOnly(14, 15),
-                5 => new TimeOnly(16, 5),
-                6 => new TimeOnly(17, 50),
-                7 => new TimeOnly(19, 35),
-                _ => new TimeOnly()
-            };
-        }
+            1 => new TimeOnly(8, 30),
+            2 => new TimeOnly(10, 20),
+            3 => new TimeOnly(12, 10),
+            4 => new TimeOnly(14, 15),
+            5 => new TimeOnly(16, 5),
+            6 => new TimeOnly(17, 50),
+            7 => new TimeOnly(19, 35),
+            _ => new TimeOnly()
+        };
     }
 }
